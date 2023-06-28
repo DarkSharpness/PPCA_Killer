f0a7769e04ebf26d98f8adcebbdbe9fcce505615
DarkSharpness_XzY <2040703891@qq.com>
2023-06-29 00:04:34 +0800

    Complete reservation station basically.

--- src/reservation.h
+++ src/reservation.h
@@ -9,58 +9,108 @@ namespace dark {
 /* Station for instructions. */
 struct reservation_station {
     struct entry {
-        byte_utype busy : 1;    /* Busy bit. */
-        code_type   op  : 7;    /* Operator bit. */
-        byte_utype idx1 : 5;    /* Index of constraint 1 order. */
-        byte_utype idx2 : 5;    /* Index of constraint 2 order.  */
+        byte_utype      : 1;
+        ALU_code    op  : 7;    /* Operator bit.     */
+        byte_utype idx1 : 5;    /* Index of constraint 1 reorder. */
+        byte_utype idx2 : 5;    /* Index of constraint 2 reorder.  */
         byte_utype dest : 5;    /* Index of destination in reorder buffer. */
         byte_utype      : 0;
-        register_type src1;     /* Source value 1. */
-        register_type src2;     /* Source value 2.  */
+        register_type  src1;    /* Source value 1. */
+        register_type  src2;    /* Source value 2.  */
         register_type result;   /* The result of reservation station. */
 
         /* Whether this entry is available to be executed. */
-        bool avail() const noexcept
-        { return busy && idx1 != 31 && idx2 != 31; }
-    };
-    static_assert(sizeof(entry) == 16);
+        bool is_ready() const noexcept { return idx1 == FREE && idx2 == FREE; }
+    }; static_assert(sizeof(entry) == 16);
 
-    entry array[32];
-    ALU_type ;
 
-    /* A wire indicating whether the arithmetic station is full. */
-    bool arith_full() const noexcept {
-        for(auto &&[unit,state] : arith)
-            if(!state.busy) return false;
-        return true;
-    }
+    [[no_unique_address]]
+    ALU_type unit[4];   /*    ALUs.     */
+    entry  array[32];   /* Entry array. */
+
+    std::bitset <32> array_state{ 0};  /* Data in array.  */
+    std::bitset <32> array_syncs{-1};  /* Array's sync data. */
+
 
     /* A wire indicating whether the arithmetic station is full. */
-    bool logic_full() const noexcept {
-        for(auto &&[unit,state] : logic)
-            if(!state.busy) return false;
-        return true;
-    }
+    bool is_full() const noexcept
+    { return array_state.size() == array_state.count(); }
 
-    /* Work in one cycle. */
+    /* Work in the cycle. */
     return_list work() noexcept {
-        return_list list; /* Return value list. */
+        return_list list; /* Return value list.     */
+        size_t __cnt = 0; /* Count of ALU occupied. */
         /* This process is actually working parrallelly. */
-        for(auto &&[unit,state] : arith) {
-            if(!state.avail()) continue;
-            auto *__p = unit.work(state.src1,state.src2,state.op);
-            if(__p) list.push_back({*__p,state.dest}),state.busy = false;
-        }
-        /* This process is actually working parrallelly. */
-        for(auto &&[unit,state] : logic) {
-            if(!state.avail()) continue;
-            auto *__p = unit.work(state.src1,state.src2,state.op);
-            if(__p) list.push_back({*__p,state.dest}),state.busy = false;
+        for(auto i  = array_state._Find_first() ;
+                 i != array_state.size() ; i = array_state._Find_next(i)) {
+            if(array[i].is_ready()) { /* Simulate the delay of 1 clock. */
+                if (array[i].op == ALU_code::WORKING) {
+                    array_syncs[i]  = false;
+                    list.push_back({array[i].result,array[i].dest});
+                } else if(__cnt != array_length(unit)) {
+                    array[i].result = unit[__cnt].work(array[i].src1,
+                                                       array[i].src2,
+                                                       array[i].op);
+                    array[i].op     = ALU_code::WORKING;
+                }
+            }
         } return list;
     }
 
-    void insert() noexcept {
+    /* Clear the pipeline. */
+    void clear_pipeline() noexcept
+    { array_state.reset(),array_syncs.set(); }
 
+
+    /**
+     * @brief Receive one command from issue.
+     * 
+     * @attention Use in the end of the cycle.
+     */
+    void insert(ALU_code __code,
+                wrapper  __reg1,
+                wrapper  __reg2,
+                register_type __dest) noexcept {
+        int __x = (~array_state)._Find_first();
+        array[__x].op   = __code;
+        array[__x].idx1 = __reg1.index();
+        array[__x].idx2 = __reg2.index();
+        array[__x].dest = __dest;
+        array[__x].src1 = __reg1.value();
+        array[__x].src2 = __reg2.value();
+    }
+
+    /**
+     * @brief Update the dependency from RoB.
+     * 
+     * @attention Use it in the end of a cycle after inserting.
+     */
+    void update(wrapper __data) noexcept {
+        for(auto i  = array_state._Find_first() ;
+                 i != array_state.size() ; i = array_state._Find_next(i)) {
+            if(array[i].idx1 == __data.index()) {
+                array[i].idx1 = FREE;
+                array[i].src1 = __data.value();
+            }
+            if(array[i].idx2 == __data.index()) {
+                array[i].idx2 = FREE;
+                array[i].src2 = __data.value();
+            }
+        }
+    }
+    
+
+    /**
+     * @brief This fucking operation is designed to 
+     * simulate real hardware, as real hardware relies
+     * on the state of last cycle.
+     * 
+     * @attention This function should only be operated
+     * in the end of the cycle.
+     */
+    void sync() {
+        array_state &= array_syncs;
+        array_syncs.set();
     }
 
 };
