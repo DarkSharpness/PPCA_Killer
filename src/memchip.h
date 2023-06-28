b5021e68b30bb21550cb60457cc553211aa1ae79
DarkSharpness_XzY <2040703891@qq.com>
2023-06-28 23:20:12 +0800

    Basically complete memory part

--- src/memchip.h
+++ src/memchip.h
@@ -12,12 +12,12 @@ struct memory_chip {
 
     /* Load a trivial type from memory. */
     template <class T>
-    void load(address_type __pos,T &__v,size_t __m = sizeof(T)) 
+    void load(address_type __pos,T &__v,size_t __m) 
     noexcept { memcpy(&__v,data + pos,__m); }
 
     /* Store a trivial type into memory. */
     template <class T>
-    void store(address_type __pos,const T &__v,size_t __m = sizeof(T)) 
+    void store(address_type __pos,const T &__v,size_t __m) 
     noexcept { memcpy(data + pos,&__v,__m); }
 
     /* Initial program data into memory from stdin. */
@@ -26,9 +26,9 @@ struct memory_chip {
         address_type __a = 0;
         while(read_token(buffer)) {
             if(buffer[0] == '@') {
-                __a = to_integer <address_type> (buffer + 1);
+                __a = hex_to_integer <address_type> (buffer + 1);
             } else {
-                data[__a] = to_integer <char> (buffer);
+                data[__a] = hex_to_integer <char> (buffer);
                 ++__a;
             }
        }
--- src/memio.h
+++ src/memio.h
@@ -3,6 +3,7 @@
 
 #include "utility.h"
 #include "memchip.h"
+#include "instruction.h"
 
 namespace dark {
 
@@ -16,36 +17,37 @@ constexpr address_type memory_size = 1 << 20;
 struct memory : memory_chip <memory_size> {
     /* Entry of one memory buffer. */
     struct entry {
-        word_utype busy   :  1; /* Busy bit || Load bit. */
+        word_utype        :  2;
+        word_utype sign   :  1; /* Load signed or unsigned. */
+        word_utype prev   :  5; /* Last store operation. */
         word_utype dest   :  5; /* Index in the reorder buffer. */
-        word_utype count  :  2; /* Counter of the clock. */
-        word_utype size   :  3; /* The length of data.  */
-        word_utype index  :  5; /* Index of constraint1.  */
-        word_utype offset : 13; /* Offset of address (with additional sign bit) */
-        word_utype        :  0;
-        register_type   source; /* The source register value. */
-        register_type   result; /* Result of the calculation. */
+        word_utype size   :  2; /* The bit_length of data.  */
+        word_utype idx1   :  5; /* Index of constraint1 in reorder. */
+        word_stype offset : 12; /* Offset of address (with additional sign bit) */
 
-        /* Whether  */
-        bool is_load() const noexcept { return busy; }
+        register_type  source1;  /* The source register value.           */
+        register_type  source2;  /* Result of the calculation or source. */
 
-        /* Whether this entry is available to be executed. */
-        bool avail() const noexcept { return busy && index != 31; }
-
+        /* Set this command as done. */
+        void set_done() noexcept       { size = 3; }
+        /* Whether this command is done. */
+        bool is_done() const noexcept  { return size == 3; }
+        /* Whether this command is available to operate. */
+        bool is_ready() const noexcept { return idx1 == FREE; }
         /* Return the real address. */
         address_type address() const noexcept
-        { return source + sign_expand <13,address_type> (offset); }
+        { return source1 + offset; }
+    }; static_assert(sizeof(entry) == 12);
 
-    };
-    static_assert(sizeof(entry) == 12);
 
-    round_queue <entry,32> loader; /* Load  buffer. */
-
-    entry current = {0,0,0,0,0,0,0,0};
-    entry tostore = {0,0,0,0,0,0,0,0};
-
-    bool sync_tag_loader = false;
-    bool sync_tag_storer = false;
+    round_queue <entry,32> loader;  /* Load  buffer. */
+    entry current;                  /* Current  entry. */
+    
+    address_type pc =   0  ;    /* PC pointer. */
+    bool load_tag   = false;    /* Whether current is load operation. */
+    byte_stype cc   =  -1  ;    /* Stupid counter...... */
+    byte_utype last = FREE ;     /* Lastest store information. */
+    byte_stype index;           /* Index of current opeartion in queue. */
 
     /**
      * @brief Inner method of fetching a command.
@@ -56,63 +58,122 @@ struct memory : memory_chip <memory_size> {
      * @param __pc The real PC value.
      * @return command_type 
      */
-    command_type fetch(address_type __pc) noexcept {
-        command_type __ret;
-        memory_chip::load(__pc,__ret);
-        return __ret;
+    command_type fetch(command_type __cmd)
+    noexcept { memory_chip::load(pc,__cmd,4); }
+
+    /* Inner handle. */
+    void work_counter() noexcept {
+        if(~cc || cc--) return;
+        /* Now cc = -1. */
+        if(load_tag) {
+            memory_chip::load(current.address(),
+                              current.source2,
+                              1 << current.size);
+            if(current.sign) {
+                if(current.size == 0)
+                    current.source2 = (int8_t)  current.source2;
+                if(current.size == 1)
+                    current.source2 = (int16_t) (current.source2);
+            } current.set_done();
+            loader[index] = current;
+        } else { /* Load into memory. */
+            memory_chip::store(current.address(),
+                               current.source2,
+                               1 << current.size);
+            int head = loader.head;
+            int size = loader.dist;
+            while(size-- && loader[head].prev == current.dest) {
+                loader[head].prev = FREE;
+                if(++head == loader.length()) head = 0;
+            } if(last == current.dest) last = FREE;
+        } /* Set done and change the index. */
     }
+
+    /* Inner handle. */
+    void clear_pipeline() noexcept
+    { loader.clear() , load_tag  = false , cc = -1 , last = FREE; }
 
     /**
-     * @brief Load one data (into queue first).
+     * @brief Work for one cycle. 
      * 
-     * @param __add Address in the memory.
-     * @param __pos Index in reorder buffer. 
+     * @return Whether load information is done.
      */
-    template <class T>
-    void load(address_type __add,byte_utype __pos,T) 
-    noexcept { loader.push({__add,0,sizeof(T),1,3,__pos}); }
-
-    /**
-     * @brief Store one data.
-     * 
-     * @param __add Address in the memory.
-     * @param __pos Index in reorder buffer. 
-     * @param __arg Argument to store into memory.
-     */
-    template <class T>
-    void store(address_type __add,byte_utype __pos,T __arg) 
-    noexcept { storer.push({__add,__arg,sizeof(T),0,3,__pos}); }
-
-    /* Load one data from memory. */
-    bool load_current() noexcept {
-        memory_chip::load (current.address(),current.result,current.size);
-        return true;
-    }
-
-    /* Store one data into memory. */
-    bool store_current() noexcept {
-        memory_chip::store(current.address(),current.result,current.size);
-        return false;
-    }
-
-    /* Work for one cycle. Return whether information should be updated. */
     wrapper work() noexcept {
-        if(!current.count) { /* Wired operation. */
-            
-        } if(current.count != 1) return {};
-        /* Start operating. */
-    
+        /* Empty and nothing doing. */
+        if(loader.empty()) return {};
+        /* Counter should decrease in this cycle. */
+        work_counter();
+        /* If head is done,then we should push now. */
+        auto head = loader.front();
+        if(!head.is_done()) return {};
+        else return {head.source2,head.dest};
     }
 
     /* A wire indicating whether the loader is full. */
-    bool loader_full() const noexcept { return loader.full(); }
+    bool is_full() const noexcept { return loader.full(); }
 
-    /* A write indicating whether the memory is writeable. */
-    bool writeable() const noexcept {
-        return current.count == 1 || /* Will be writeable or empty. */
-              (current.count == 0 && loader.empty());
+    /**
+     * @brief Insert a command to the queue.
+     * 
+     * @attention Use it in the end of a cycle.
+     */
+    inline void insert (word_utype __sign,
+                        word_utype __dest,
+                        word_utype __length,
+                        word_stype __offset,
+                        wrapper    __reg1) noexcept {
+        loader.push({
+                    __sign,
+                    last,
+                    __dest,
+                    __length,
+                    __reg1.idx,
+                    __offset,
+                    __reg1.val,
+                    0
+                });
     }
 
+    /* Special insert for store command. */
+    void insert_store(word_utype __dest) { last = __dest; } 
+
+    /**
+     * @brief Store data when store command is commited.
+     * 
+     * @attention Use it in the end of a cycle.
+     */
+    void store (word_utype  __dest  ,
+                word_utype  __length,
+                word_stype  __offset,
+                register_type __reg1,
+                register_type __reg2) noexcept {
+        load_tag = false , cc = 2;
+        current.dest    = __dest;
+        current.size    = __length;
+        current.offset  = 0;
+        current.source1 = __reg1;
+        current.source2 = __reg2;
+    }
+
+    /**
+     * @brief Update the dependency from RoB.
+     * 
+     * @attention Use it after insertion.
+     */
+    void update(wrapper __data) noexcept {
+        int head = loader.head;
+        int size = loader.dist;
+        if(!__data.is_file()) {
+            if(__data.is_jump_WA()) clear_pipeline();
+            return; /* BRANCH || JALR || STORE. */
+        } while(size--) {
+            auto &__c = loader[head];
+            if(__c.idx1 == __data.idx) {
+                __c.idx1    = FREE;
+                __c.source1 = __data.val;
+            }
+        }
+    }
 
 
     /**
@@ -120,11 +181,30 @@ struct memory : memory_chip <memory_size> {
      * simulate real hardware, as real hardware relies
      * on the state of last cycle.
      * 
+     * @attention This function should only be operated
+     * in the end of a cycle.
      */
     void sync() {
-        if(current.count) --current.count;
-        if(sync_tag_loader) loader.pop() , sync_tag_loader = false;
+        /* Is calculating || Nothing can be done. */
+        if(cc != -1 || loader.empty()) return;
+
+        /* Pop out the first load element if done. */
+        if(loader.front().is_done()) loader.pop();
+
+        /* Tries to load a new element. */
+        int head = loader.head;
+        int size = loader.dist;
+        while(size-- && loader[head].prev == FREE) {
+            if(loader[head].is_ready() && !loader[head].is_done()) {
+                load_tag = true, cc = 2;
+                current  = loader[head];
+                index    = head;
+                return;
+            } if(++head == loader.length()) head = 0;
+        }
     }
+
+
 
 };
 
