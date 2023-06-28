9038a65a23e72da4c618fd8f1f598dd5090084c1
DarkSharpness_XzY <2040703891@qq.com>
2023-06-28 22:05:16 +0800

    Complete ALU and round_queue

--- src/alu.h
+++ src/alu.h
@@ -5,25 +5,33 @@
 
 namespace dark {
 
-/* Base class for ALU. */
+/**
+ * @brief Custom class for ALU operation.
+ * Note that it is implemented as an empty class.
+ * You should use [[no_unique_address]] to save space.
+ * 
+ */
 struct arithetic_logic_unit {
-    register_type result; /* Result of current operation. */
-
-    register_type work (register_type __reg1,
-                        register_type __reg2,
-                        alu_code      __code) noexcept {
+    /* It is actually empty class which requires no this pointer. */
+    static register_type work (register_type __reg1,
+                               register_type __reg2,
+                               ALU_code      __code) noexcept {
         switch(__code) {
-            case alu_code::add : return             __reg1  + __reg2;
-            case alu_code::sub : return             __reg1  - __reg2;
-            case alu_code::sll : return (word_utype)__reg1 << __reg2;
-            case alu_code::srl : return (word_utype)__reg1 >> __reg2;
-            case alu_code::sra : return (word_stype)__reg1 >> __reg2;
-            case alu_code::slt : return (word_stype)__reg1  < (word_stype)__reg2;
-            case alu_code::sltu: return (word_utype)__reg1  < (word_utype)__reg2;
-            case alu_code::xor_: return             __reg1  ^ __reg2;
-            case alu_code::or_ : return             __reg1  | __reg2;
-            case alu_code::and_: return             __reg1  & __reg2;
-            default: ;/* This should never happen. */
+            case ALU_code::ADD : return             __reg1  + __reg2;
+            case ALU_code::SUB : return             __reg1  - __reg2;
+            case ALU_code::ALL : return (word_utype)__reg1 << __reg2;
+            case ALU_code::SRL : return (word_utype)__reg1 >> __reg2;
+            case ALU_code::SRA : return (word_stype)__reg1 >> __reg2;
+            case ALU_code::LT  : return (word_stype)__reg1  < (word_stype)__reg2;
+            case ALU_code::LTU : return (word_utype)__reg1  < (word_utype)__reg2;
+            case ALU_code::GE  : return (word_stype)__reg1 >= (word_stype)__reg2;
+            case ALU_code::GEU : return (word_utype)__reg1 >= (word_utype)__reg2;
+            case ALU_code::EQ  : return             __reg1 == __reg2;
+            case ALU_code::NE  : return             __reg1 != __reg2;
+            case ALU_code::XOR: return             __reg1  ^ __reg2;
+            case ALU_code::OR : return             __reg1  | __reg2;
+            case ALU_code::AND: return             __reg1  & __reg2;
+            default: throw;/* This should never happen. */
         }
     }
 };
--- src/round_queue.h
+++ src/round_queue.h
@@ -14,14 +14,16 @@ struct round_queue {
     void clear() noexcept { head = dist = 0; }
 
     /* Force to push an element to the back of the queue. */
-    void push(const T &__v) noexcept {
-        int __x = head + dist;
-        data[__x >= __n ? __x - __n : __x] = __v;
-        ++dist;
-    }
+    void push(const T &__v) noexcept { data[tail()] = __v; ++dist; }
 
     /* Pop an element from the front of the queue. */
-    void pop() noexcept { ++head,--dist; if(head == __n) head = 0; }
+    void pop() noexcept { --dist; if(++head == __n) head = 0; }
+
+    /* Return the tail position ready to be inserted. */
+    int tail() const noexcept {
+        int __x = head + dist;
+        return __x >= __n ? __x - __n : __x;
+    }
 
     T &operator [](int x) noexcept { return data[x]; }
 
@@ -30,24 +32,12 @@ struct round_queue {
     /* Return const reference to the first element. */
     const T &front() const noexcept { return data[head]; }
 
-    /* Return reference to the last element. */
-    T &back () noexcept {
-        int __x = head + dist;
-        return data[__x >= __n ? __x - __n : __x];
-    }
-
-    /* Return reference to the l */
-    const T &back () const noexcept {
-        int __x = head + dist;
-        return data[__x >= __n ? __x - __n : __x];
-    }
-
     /* Return the length of the queue. */
     constexpr int length() const noexcept { return __n; }
     /* Return whether the queue is full. */
     bool full() const noexcept { return dist == length(); }
     /* Return whether the queue contains element. */
-    int  size() const noexcept { return  dist; }
+    auto size() const noexcept { return  dist; }
     /* Return whether the queue is empty. */
     bool empty()const noexcept { return !dist; }
 
