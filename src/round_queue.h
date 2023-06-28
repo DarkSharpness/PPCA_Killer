fb5f92beaf448c145154b4d7ee072efeea32f477
DarkSharpness_XzY <2040703891@qq.com>
2023-06-28 11:07:42 +0800

    Restructure round queue

--- src/round_queue.h
+++ src/round_queue.h
@@ -3,28 +3,25 @@
 
 namespace dark {
 
+
 template <class T,int __n>
 struct round_queue {
     T data[__n];
     int head = 0;
-    int tail = 0;
+    int dist = 0;
 
     /* Clear all the elements in the queue. */
-    void clear() noexcept { head = tail = 0; }
+    void clear() noexcept { head = dist = 0; }
 
     /* Force to push an element to the back of the queue. */
     void push(const T &__v) noexcept {
-        data[tail++] = __v;
-        if(tail == __n)  tail =  0;
-        if(tail == head) tail = -1;
+        int __x = head + dist;
+        data[__x >= __n ? __x - __n : __x] = __v;
+        ++dist;
     }
 
     /* Pop an element from the front of the queue. */
-    void pop() noexcept {
-        if(tail == -1)   tail = head;
-        ++head;
-        if(head == __n)  head = 0;
-    }
+    void pop() noexcept { ++head,--dist; if(head == __n) head = 0; }
 
     T &operator [](int x) noexcept { return data[x]; }
 
@@ -33,14 +30,27 @@ struct round_queue {
     /* Return const reference to the first element. */
     const T &front() const noexcept { return data[head]; }
 
-    /* Return whether the queue is full. */
-    bool full() const noexcept { return tail == -1  ; }
-    /* Return whether the queue contains element. */
-    bool size() const noexcept { return head != tail; }
-    /* Return whether the queue is empty. */
-    bool empty()const noexcept { return head == tail; }
+    /* Return reference to the last element. */
+    T &back () noexcept {
+        int __x = head + dist;
+        return data[__x >= __n ? __x - __n : __x];
+    }
 
+    /* Return reference to the l */
+    const T &back () const noexcept {
+        int __x = head + dist;
+        return data[__x >= __n ? __x - __n : __x];
+    }
+
+    /* Return the length of the queue. */
     constexpr int length() const noexcept { return __n; }
+    /* Return whether the queue is full. */
+    bool full() const noexcept { return dist == length(); }
+    /* Return whether the queue contains element. */
+    int  size() const noexcept { return  dist; }
+    /* Return whether the queue is empty. */
+    bool empty()const noexcept { return !dist; }
+
 };
 
 }
